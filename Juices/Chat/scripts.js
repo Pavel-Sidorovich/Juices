b1005c0295052d0632d65b492aba245144467797
Pavel Sidorovich <sidorovichpavelalex@gmail.com>
2015-03-16 21:14:57 +0400

    Practice 4_1

--- Juices/Chat/scripts.js
+++ Juices/Chat/scripts.js
@@ -4,12 +4,23 @@ function sendClick() {
     if (TheTextBoxTwo.value && TheTextBoxOne.value) {
         var parentElem = document.body.children[3];
         var d = document.createElement('div');
-        //d.id = 
+        d.setAttribute("onclick", 'select(this)');
         d.nameId = TheTextBoxTwo.value;
         d.innerHTML = TheTextBoxTwo.value + ': ' + TheTextBoxOne.value;
         parentElem.appendChild(d);
     }
     TheTextBoxOne.value = null;
+}
+
+function select(el) {
+    if (el.style.color == 'black') {
+        el.style.background = 'LightSlateGray';
+        el.style.color = 'red';
+    }
+    else {
+        el.style.background = 'Cornsilk';
+        el.style.color = 'black';
+    }
 }
 
 function remove(elem) {
@@ -22,11 +33,11 @@ function deleteClick() {
     var children = document.getElementById('block2').childNodes;
     var count = 0;
     for(var i=0;i<children.length; i++) {
-        if (window.getSelection().containsNode(children[i], true)) {
+        if (children[i].style.color == 'red') {
             var d2 = children[i];
             count += 1;
         }
-    }
+        }
     if (count == 1)
         if (d2.nameId == TheTextBoxTwo.value)
             remove(d2);
@@ -36,15 +47,15 @@ function renameClick() {
     var TheTextBoxTwo = document.getElementById('entername');
     var TheTextBoxOne = document.getElementById('entermessages');
     var d = document.createElement('div');
+    d.setAttribute("onclick", 'select(this)');
     d.nameId = TheTextBoxTwo.value;
     var count = 0;
     d.innerHTML = TheTextBoxTwo.value + ': ' + TheTextBoxOne.value;
     var children = document.getElementById('block2').childNodes;
     for (var i = 0; i < children.length; i++) {
-        if (window.getSelection().containsNode(children[i], true)) {
+        if (children[i].style.color == 'red') {
             var d2 = children[i];
             count += 1;
-
         }
     }
     if (TheTextBoxTwo.value && TheTextBoxOne.value) {
@@ -60,3 +71,27 @@ function rename(elem, d) {
     parentElement.insertBefore(d, elem);
     parentElement.removeChild(elem);
 }
+//function store(listToSave) {
+
+//    var listAsString = JSON.stringify(listToSave);
+
+//    localStorage.setItem("TODOs taskList",listAsString);
+
+////}
+
+//function restore() {
+
+//    var item = localStorage.getItem("halma.piece.1.row");
+//    {
+//        var parentElem = document.body.children[3];
+//        var d = document.createElement('div');
+//        d.nameId = item.nameId;
+//        d.innerHTML = item.innerHTML;
+//        parentElem.appendChild(d);
+//        store(d);
+//    }
+//    var TheTextBoxTwo = document.getElementById('entername');
+//    TheTextBoxTwo = item;
+//    //return item && JSON.parse(item);
+
+//}
